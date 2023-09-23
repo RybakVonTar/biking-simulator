7c4ae0703f8aedc85ed41a68259c188f45c9f626
Rustatano <jakub-svoboda111@seznam.cz>
2023-09-23 20:42:49 +0200

    bike jump
    - just a prototype it can be change in the future
    - added invisible wall on the far of map left

--- Biking Simulator/Assets/Scripts/bike/BikeFrame.cs
+++ Biking Simulator/Assets/Scripts/bike/BikeFrame.cs
@@ -1,9 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class BikeFrame : MonoBehaviour {
 
     public int speed;
+    public int jumpForce;
+    public float tiltForce;
+    private Rigidbody2D rb;
+    public Vector3 boxSize;
+    public float maxDistance;
+    public LayerMask layerMask;
 
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && GroundCheck()) {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            // tilting
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            // tilting
+        }
+    }
+
+    private bool GroundCheck() {
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, maxDistance, layerMask)) {
+            return true;
+        }
+        return false;
+    }
 }
