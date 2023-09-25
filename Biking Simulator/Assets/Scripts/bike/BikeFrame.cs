832a75e5c5f6d9cb2ad8babef72c910772066ff6
Rustatano <jakub-svoboda111@seznam.cz>
2023-09-25 09:18:02 +0200

    better mod air control

--- Biking Simulator/Assets/Scripts/bike/BikeFrame.cs
+++ Biking Simulator/Assets/Scripts/bike/BikeFrame.cs
@@ -11,10 +11,10 @@ public class BikeFrame : MonoBehaviour {
     public int jumpForce;
     public float tiltAcceleration;
     private float tiltForce;
+    public float maxTiltForce;
     private Rigidbody2D rb;
-    public Vector3 boxSize;
-    public float maxDistance;
-    public LayerMask layerMask;
+    public BikeWheels frontWheel;
+    public BikeWheels backWheel;
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -22,29 +22,22 @@ public class BikeFrame : MonoBehaviour {
 
     void Update() {
         
-        if (Input.GetKeyDown(KeyCode.Space) && GroundCheck()) {
+        if (Input.GetKeyDown(KeyCode.Space) && (backWheel.GroundCheck() || frontWheel.GroundCheck())) {
             rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
             tiltForce = 0;
         }
 
-        if (Input.GetKey(KeyCode.A) && !GroundCheck()) {
-            if (tiltForce < 2) {
+        if (Input.GetKey(KeyCode.A) && !(backWheel.GroundCheck() || frontWheel.GroundCheck())) {
+            if (tiltForce < maxTiltForce) {
                 tiltForce += tiltAcceleration;
             }
             transform.Rotate(0, 0, tiltForce);
 
-        } else if (Input.GetKey(KeyCode.D) && !GroundCheck()) {
-            if (tiltForce < 2) {
+        } else if (Input.GetKey(KeyCode.D) && !(backWheel.GroundCheck() || frontWheel.GroundCheck())) {
+            if (tiltForce < maxTiltForce) {
                 tiltForce += tiltAcceleration;
             }
             transform.Rotate(0, 0, -tiltForce);
         }
-    }
-
-    private bool GroundCheck() {
-        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, maxDistance, layerMask)) {
-            return true;
-        }
-        return false;
     }
 }
--- Biking Simulator/Assets/Scripts/bike/BikeWheels.cs
+++ Biking Simulator/Assets/Scripts/bike/BikeWheels.cs
@@ -6,6 +6,9 @@ public class BikeWheels : MonoBehaviour {
 
     public BikeFrame bike;
     private HingeJoint2D hj;
+    public Vector3 boxSize;
+    public float maxDistance;
+    public LayerMask layerMask;
 
     void Start() {
         hj = GetComponent<HingeJoint2D>();
@@ -17,5 +20,11 @@ public class BikeWheels : MonoBehaviour {
         var motor = hj.motor;
         motor.motorSpeed = horizontalInput * bike.speed;
         hj.motor = motor;
+    }
+    public bool GroundCheck() {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, maxDistance, layerMask)) {
+            return true;
+        }
+        return false;
     }
 }
